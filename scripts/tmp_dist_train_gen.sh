0f8e71d38c1da1cb252adb1d44f0ef14caec58b6
qiaozhijian <935514540@qq.com>
2023-02-13 02:05:11 +0800

    update

--- scripts/tmp_dist_train_gen.sh
+++ scripts/tmp_dist_train_gen.sh
@@ -6,15 +6,16 @@ export OMP_NUM_THREADS=24
 export EXPR_NAME=GenLaneNet
 export NUM_EPOCHS=40
 
-while true
-do
-    PORT=$(( ((RANDOM<<15)|RANDOM) % 49152 + 10000 ))
-    status="$(nc -z 127.0.0.1 $PORT < /dev/null &>/dev/null; echo $?)"
-    if [ "${status}" != "0" ]; then
-        break;
-    fi
-done
-echo $PORT
+#while true
+#do
+#    PORT=$(( ((RANDOM<<15)|RANDOM) % 49152 + 10000 ))
+#    status="$(nc -z 127.0.0.1 $PORT < /dev/null &>/dev/null; echo $?)"
+#    if [ "${status}" != "0" ]; then
+#        break;
+#    fi
+#done
+#echo $PORT
+PORT=25750
 python -m torch.distributed.launch --master_port=$PORT --nproc_per_node ${NUM_GPUS} main_genlanenet.py \
       --mod=${EXPR_NAME} \
       --nepochs=${NUM_EPOCHS} \
