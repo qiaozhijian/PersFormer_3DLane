3911d7f9ff33e64bd64e2e5d2af712c3528c3e2a
qiaozhijian <935514540@qq.com>
2023-02-13 01:59:45 +0800

    update

--- scripts/tmp_dist_train_gen.sh
+++ scripts/tmp_dist_train_gen.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 #sh scripts/tmp_dist_train_gen.sh 0,1,2,3 4 --batch_size=8
-export CUDA_VISIBLE_DEVICES=$1
-export NUM_GPUS=$2
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4
+export NUM_GPUS=5
 export OMP_NUM_THREADS=24
 export EXPR_NAME=GenLaneNet
 export NUM_EPOCHS=40
-PY_ARGS=${@:3}
 
 while true
 do
@@ -19,4 +18,4 @@ echo $PORT
 python -m torch.distributed.launch --master_port=$PORT --nproc_per_node ${NUM_GPUS} train.py \
       --mod=${EXPR_NAME} \
       --nepochs=${NUM_EPOCHS} \
-      ${PY_ARGS}
+      --batch_size=8
